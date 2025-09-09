2c918db592bf955d771dc4996b1a896c10a7fcdb
mrMadMaster <orlov1988brest@gmail.com>
2025-09-09 16:30:25 +0300

    Задача спринта №10

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,37 @@
+package com.example.playlistmaker
+
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class TrackViewHolder(item: View): RecyclerView.ViewHolder(item) {
+
+    private val trackName: TextView = item.findViewById(R.id.track_name)
+    private val artistName: TextView = item.findViewById(R.id.artist_name)
+    private val trackTime: TextView = item.findViewById(R.id.track_time)
+    private val artwork: ImageView = item.findViewById(R.id.artwork)
+
+    fun bind(track: Track){
+        trackName.text = track.trackName
+        artistName.text = track.artistName
+        trackTime.text = track.trackTime
+
+        Glide.with(itemView)
+            .load(track.artworkUrl100)
+            .centerCrop()
+            .placeholder(R.drawable.placeholder_45)
+            .transform(RoundedCorners(dpToPx(2f, itemView)))
+            .into(artwork)
+    }
+
+    private fun dpToPx(dp: Float, context: View): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics).toInt()
+    }
+}
