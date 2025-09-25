5124013e7dad8b3858e1273a8661f857ed27d2cb
mrMadMaster <orlov1988brest@gmail.com>
2025-09-25 16:35:13 +0300

    Задача спринта №11

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,65 +4,76 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
 
     private var currentEditText: String = EDITTEXT_DEF
 
-    private val trackList = listOf(
-        Track(
-            trackName = "Smells Like Teen Spirit",
-            artistName = "Nirvana",
-            trackTime = "5:01",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Billie Jean",
-            artistName = "Michael Jackson",
-            trackTime = "4:35",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Stayin' Alive",
-            artistName = "Bee Gees",
-            trackTime = "4:10",
-            artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Whole Lotta Love",
-            artistName = "Led Zeppelin",
-            trackTime = "5:33",
-            artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Sweet Child O'Mine",
-            artistName = "Guns N' Roses",
-            trackTime = "5:03",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://itunes.apple.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val itunesService = retrofit.create(ItunesApi::class.java)
+    private lateinit var inputEditText: EditText
+    private lateinit var nothingFound: LinearLayout
+    private lateinit var noConnection: LinearLayout
+
+
+    private val trackList = mutableListOf<Track>()
+    val trackAdapter = TrackAdapter(trackList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-
         val backButton = findViewById<ImageButton>(R.id.back)
         val clearButton = findViewById<ImageView>(R.id.clear)
-        val inputEditText = findViewById<EditText>(R.id.searchText)
+        inputEditText = findViewById(R.id.searchText)
+        nothingFound = findViewById(R.id.nothingFound)
+        noConnection = findViewById(R.id.noConnection)
+        val refreshButton = findViewById<Button>(R.id.refreshButton)
+        val recycler = findViewById<RecyclerView>(R.id.tracks)
+        recycler.adapter = trackAdapter
 
         backButton.setOnClickListener { finish() }
 
         clearButton.setOnClickListener {
             inputEditText.text.clear()
+            noConnection.visibility = View.GONE
+            nothingFound.visibility = View.GONE
+            trackList.clear()
+            trackAdapter.notifyDataSetChanged()
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(inputEditText.windowToken, 0)
             inputEditText.clearFocus()
+        }
+
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                trackList.clear()
+                trackAdapter.notifyDataSetChanged()
+                search()
+                true
+            }
+            false
+        }
+
+        refreshButton.setOnClickListener {
+            search()
         }
 
         val simpleTextWatcher = object : TextWatcher {
@@ -71,16 +82,54 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                currentEditText = s.toString()
                 clearButton.visibility = clearButtonVisibility(s)
             }
 
             override fun afterTextChanged(s: Editable?) {
-                currentEditText = s.toString()
-                val recycler = findViewById<RecyclerView>(R.id.tracks)
-                recycler.adapter = TrackAdapter(trackList)
+
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun search() {
+        if (inputEditText.text.isNotEmpty()) {
+            itunesService.findTrack(inputEditText.text.toString()).enqueue(object :
+                Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        val results = response.body()?.results
+                        if (results?.isNotEmpty() == true) {
+                            noConnection.visibility = View.GONE
+                            nothingFound.visibility = View.GONE
+                            trackList.addAll(results)
+                            trackAdapter.notifyDataSetChanged()
+                        } else {
+                            noConnection.visibility = View.GONE
+                            nothingFound.visibility = View.VISIBLE
+                            trackList.clear()
+                            trackAdapter.notifyDataSetChanged()
+                        }
+                    } else {
+                        noConnection.visibility = View.VISIBLE
+                        nothingFound.visibility = View.GONE
+                        trackList.clear()
+                        trackAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    noConnection.visibility = View.VISIBLE
+                    nothingFound.visibility = View.GONE
+                    trackList.clear()
+                    trackAdapter.notifyDataSetChanged()
+                }
+            })
+        }
     }
 
     fun clearButtonVisibility(s: CharSequence?): Int {
--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -7,6 +7,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackViewHolder(item: View): RecyclerView.ViewHolder(item) {
 
@@ -18,7 +20,7 @@ class TrackViewHolder(item: View): RecyclerView.ViewHolder(item) {
     fun bind(track: Track){
         trackName.text = track.trackName
         artistName.text = track.artistName
-        trackTime.text = track.trackTime
+        trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis.toLong())
 
         Glide.with(itemView)
             .load(track.artworkUrl100)
