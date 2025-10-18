5bff953562f9a132ae1d782f2765450a3a8c93f6
mrMadMaster <orlov1988brest@gmail.com>
2025-10-18 14:07:48 +0300

    Задача спринта №13

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,9 +1,17 @@
 package com.example.playlistmaker
 
+import java.io.Serializable
+
 data class Track(
     val trackName: String,
     val artistName: String,
     val trackTimeMillis: String,
     val artworkUrl100: String,
-    val trackId: Int
-)
+    val trackId: Int,
+    val collectionName: String?,
+    val releaseDate: String?,
+    val primaryGenreName: String?,
+    val country: String?
+) : Serializable {
+    fun getCoverArtwork(): String = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+}
